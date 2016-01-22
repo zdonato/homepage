0a373a250f99d8a0a3f95ecf05e38ec2f803f524
Zachary Donato <zdonato@stevens.edu>
2016-01-21 23:26:05 -0500

    Working on weather.

--- js/Controllers/HomePageController.js
+++ js/Controllers/HomePageController.js
@@ -2,12 +2,44 @@
  * Created by zdonato on 1/20/2016.
  */
 
-homepage.controller('HomePageController', ['$scope', '$http',
-    function ($scope, $http) {
+homepage.controller('HomePageController', ['$scope', '$http', '$interval',
+    function ($scope, $http, $interval) {
 
         $http.get('js/json/links.json')
             .success( function (response) {
                 $scope.links = response;
-            })
+            });
+
+        $http.get('apiKey.txt')
+            .success( function (response) {
+                this.apiKey = response;
+
+                getWeather();
+            });
+
+        var update = function () {
+            $scope.currDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+        };
+
+        var getWeather = function() {
+
+            $http.get('http://api.wunderground.com/api/2951699d2fd11ad7/forecast/geolookup/conditions/q/NJ/Hoboken.json')
+                .success(function (response) {
+                    var forecast = response.forecast.simpleforecast.forecastday[0];
+
+                    $scope.weather = {
+                        high : forecast.high.fahrenheit,
+                        low: forecast.low.fahrenheit,
+                        condition : forecast.conditions,
+                        icon : forecast.icon_url
+                    }
+                })
+                .error( function (error) {
+
+                });
+        };
+
+        update();
+        $interval(update, 1000);
     }
 ]);
