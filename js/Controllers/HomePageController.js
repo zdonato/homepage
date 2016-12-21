dcf7d68d26235d34f3acca91af9479e983d5bffd
zdonato <zdonato@stevens.edu>
2016-12-21 18:24:34 -0500

    Added location support for weather services.

--- js/Controllers/HomePageController.js
+++ js/Controllers/HomePageController.js
@@ -14,16 +14,43 @@ homepage.controller('HomePageController', ['$scope', '$http', '$interval',
             .success( function (response) {
                 this.apiKey = response;
 
-                getWeather();
+                // Get the current coordinates.
+                navigator.geolocation.getCurrentPosition( function (pos) {
+                    var lat = pos.coords.latitude,
+                        long = pos.coords.longitude;
+
+                    getLocation(lat, long)
+                        .then( function (obj) {
+
+                            console.log(obj.state, obj.city);
+                            getWeather(obj.state, obj.city);
+                        })
+                });
             });
 
         var update = function () {
             $scope.currDate = moment().format('MMMM Do YYYY, h:mm:ss a');
         };
 
-        var getWeather = function() {
+        var getLocation = function(lat, long) {
+            return new Promise( function(resolve, reject) {
+                $http.get('http://api.wunderground.com/api/' + this.apiKey + '/geolookup/q/' + lat + ',' + long + '.json')
+                    .success( function (response) {
+                        var obj = {
+                            city: response.location.city,
+                            state: response.location.state
+                        };
 
-            $http.get('http://api.wunderground.com/api/' + this.apiKey + '/forecast/geolookup/conditions/q/NJ/Hoboken.json')
+                        $scope.city = obj.city;
+
+                        resolve(obj);
+                    })
+            });
+        };
+
+        var getWeather = function(state, city) {
+
+            $http.get('http://api.wunderground.com/api/' + this.apiKey + '/forecast/geolookup/conditions/q/' + state + '/' + city + '/.json')
                 .success(function (response) {
                     var forecast = response.forecast.simpleforecast.forecastday[0];
 
@@ -35,7 +62,7 @@ homepage.controller('HomePageController', ['$scope', '$http', '$interval',
                         currentIcon : response.current_observation.icon_url,
                         currentCondition : response.current_observation.weather,
                         currentTemp : response.current_observation.temp_f,
-                        forecastLink : response.current_observation.forecast_url,
+                        forecastLink : response.current_observation.forecast_url + '?apiref=d2081443386915b3',
                         feelsLike : response.current_observation.feelslike_f
                     }
                 })
